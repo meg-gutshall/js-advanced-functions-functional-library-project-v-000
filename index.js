0ab9a6eca708e692430f6f23cd3178573bc0076b
Meg Gutshall <meghan.gutshall@gmail.com>
2020-03-22 01:18:40 -0400

    Work on each method

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function(collection, alert) {
-      let newCollection = collection.isArray ? collection : collection.values;
-      newCollection.forEach(value => alert(value));
+    each: function(collection, callback) {
+      if (collection.isArray) {
+        let newCollection = collection;
+        for (const element of newCollection) {
+          callback(element);
+        }
+      } else {
+        let newCollection = collection;
+        for (const element in newCollection) {
+          callback(element);
+        }
+      }
       return collection;
     },
 
